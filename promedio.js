d8a9b545c259d48b2e2d32e87046f02eb527bb63
VICENTE AVILA <91331048+Vicente68@users.noreply.github.com>
2021-10-13 19:07:24 +0000

    hice el programa para calcular el promedio y añadi el boton de enviar

---
+++ promedio.js
@@ -0,0 +1,19 @@
+function calcularPromedio()  { // 1. inicio 
+  // 2. Definir c1, c2, c3, c4, c5, promedio 
+  var c1;
+  var c2;
+  var c3;
+  var c4;
+  var c5;
+  var promedio;
+  // 3. Hacer c1 =  10, c2 = 7.5, c3 = 9, c4 = 10, c5 = 10
+  c1 = 10;
+  c1 = 7.5;
+  c1 = 9;
+  c1 = 10;
+  c1 = 10;
+  // 4. Hcer promedio = c1 + c2 + c3 + c4 + c5 / 5 = 46.5 / 5 = 9.3
+  promedio = c1 + c2 + c3 + c4 + c5 / 5;
+  // 5. Imprimir promedio 
+  alert("Tu promedio es de; " + promdeio );
+} // 6. Fin 
